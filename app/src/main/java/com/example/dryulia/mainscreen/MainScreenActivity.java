dcd50e8877fb1f9110148b1cefe14aea1d1576de
insan nurz <gamersg801@gmail.com>
2019-04-07 20:25:39 +0700

    building structure

---
+++ app/src/main/java/com/example/dryulia/mainscreen/MainScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.dryulia.mainscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dryulia.R;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
