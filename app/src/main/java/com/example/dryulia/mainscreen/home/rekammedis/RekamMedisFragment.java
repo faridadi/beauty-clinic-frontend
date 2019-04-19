1d14684389130771c793054d7076a8e03e530daa
insan nurz <gamersg801@gmail.com>
2019-04-19 15:00:45 +0700

    scroll register

---
+++ app/src/main/java/com/example/dryulia/mainscreen/home/rekammedis/RekamMedisFragment.java
@@ -0,0 +1,37 @@
+package com.example.dryulia.mainscreen.home.rekammedis;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dryulia.R;
+
+
+public class RekamMedisFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rekam_medis, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
