00be6dfb4d364b8faca07fd3df7be53be92d751d
insan nurz <gamersg801@gmail.com>
2019-04-19 18:04:11 +0700

    bill tanggal belum dynamic

---
+++ app/src/main/java/com/example/dryulia/mainscreen/home/bill/TableBillFragment.java
@@ -0,0 +1,31 @@
+package com.example.dryulia.mainscreen.home.bill;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dryulia.R;
+
+
+public class TableBillFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_table_bill, container, false);
+    }
+
+
+}
