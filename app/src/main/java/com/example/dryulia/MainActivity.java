8e0191bf8903cf1b8d5d3bfb0d938275475f3840
insan nurz <gamersg801@gmail.com>
2019-04-20 13:05:24 +0700

    splash screen

--- app/src/main/java/com/example/dryulia/MainActivity.java
+++ app/src/main/java/com/example/dryulia/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.dryulia;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.dryulia.auth.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent LoginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(LoginIntent);
+                finish();
+            }
+        }, 3000);
     }
 }
